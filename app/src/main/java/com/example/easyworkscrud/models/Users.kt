aba0e356bdee0c8fd3a5d9a84b2c6ef2a5e887be
IT21326240 <it21326240@my.sliit.lk>
2023-05-04 21:15:23 +0530

    commit

---
+++ app/src/main/java/com/example/easyworkscrud/models/Users.kt
@@ -0,0 +1,14 @@
+package com.example.easyworkscrud.models
+
+class Users (
+
+    var firstname: String? = null,
+    var lastname: String? = null,
+    var address: String? = null,
+    var phone: String? = null,
+    var email: String? = null,
+    var checkbox: String? = null,
+    var uid: String? = null
+
+        )
+
